6e85824063008bd8df76df7c31dfc98f5a91357b
david-zucker <david.zucker@mail.huji.ac.il>
2024-12-03 18:48:11 +0200

    Add files via upload

--- boot1.r
+++ boot1.r
@@ -1,18 +1,20 @@
 #PROGRAM TO DEMONSTRATE ASYMPTOTIC CI WITH BOOTSTRAP VARIANCE
 #PARAMETER OF INTEREST IS COEFFICIENT OF VARIATION SD(Y)/E[Y]
 
+#gamma distribution with Wasserman parameterization
+
 #PRELIMINARIES
 set.seed(342345)
 za = -qnorm(0.025)
 n = 50
 alftru = 3
-bettru = 2
+bettru = 0.5
 M = 200
 
 #TRUE PARAMETER VALUE is 1/sqrt(alftru) = 0.5774
 
 #GENERATE SAMPLE OF SIZE n FROM GAMMA(alftru,bettru) DISTRIBUTION
-yorig = rgamma(n,alftru,rate=bettru)
+yorig = rgamma(n,alftru,scale=bettru)
 
 #COMPUTE ESTIMATES OF THETA FROM THE ORIGINAL DATA
 thhat = sd(yorig)/mean(yorig)
