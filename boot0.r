711283e74989fc3d70efc6fb9e9dd9d0b04b08df
david-zucker <david.zucker@mail.huji.ac.il>
2024-12-03 18:39:36 +0200

    Add files via upload

--- boot0.r
+++ boot0.r
@@ -1,27 +1,30 @@
+#DEMO OF USING SIMULATION FOR PROBABILITY CALCULATION
+
+#gamma distribution with Wasserman parameterization
+
 #SETUP
 set.seed(342345)
 n = 20
 alftru = 0.25
 bettru = 1
-thtru = alftru/bettru
 M = 100000
 c = 0.225
 
 #EXACT CALCULATION
 alfnew = n*alftru
-betnew = n*bettru
-exactp = pgamma(c,alfnew,betnew)
+betnew = bettru/n
+exactp = pgamma(c,alfnew,scale=betnew)
 
 #ASYMPTOTIC CALCULATION
-expval = alftru/bettru
-varval = alftru/(bettru^2)
+expval = alftru*bettru
+varval = alftru*(bettru^2)
 z = sqrt(n)*(c-expval)/sqrt(varval)
 asyp = pnorm(z)
 
 #SIMULATION CALCULATION
 ybar = rep(0,M)
 for (m in 1:M) {
-  y = rgamma(n,alftru,rate=bettru)
+  y = rgamma(n,alftru,scale=bettru)
   ybar[m] = mean(y)
  }
 indic = (ybar <= c)
